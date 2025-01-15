cf236b5210573139d5f2d6c352c7515a5191473c
geraldo ferreira <geraldosousadev@gmail.com>
2025-01-15 08:24:12 -0300

    searching for website title

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0'}
-url = 'https://www.climatempo.com.br/'
 
-page = requests.get(url, headers=headers)
+def teste():
 
-soup = BeautifulSoup(page.content, 'html.parser')
+    headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0'}
+    url = 'https://www.climatempo.com.br/previsao-do-tempo/cidade/6699/saopedrodopiaui-pi'
+
+    page = requests.get(url, headers=headers)
+    soup = BeautifulSoup(page.text, 'html.parser')
+
+    # print(soup.prettify())
+
+    title = soup.find('h1').text.replace('\t', '').replace('\n','')
+    
+
+teste()
